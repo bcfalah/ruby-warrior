e2de99e48559e3163635f47915098cdaf04c385c
Brian Cohen Falah <briancf@gmail.com>
2017-11-06 21:55:01 -0300

    Intermediate level 3 passed, can be improved

--- rubywarrior/falah-intermediate/player.rb
+++ rubywarrior/falah-intermediate/player.rb
@@ -1,10 +1,32 @@
+require 'byebug'
+
 class Player
+  @all_enemies_bound = false
+
   def play_turn(warrior)
-    stairs_direction = warrior.direction_of_stairs
-    if warrior.feel(stairs_direction).enemy?
-      warrior.attack!(stairs_direction)
-    else
-      warrior.walk!(stairs_direction)
+    if !bind_near_enemies(warrior)
+      stairs_direction = warrior.direction_of_stairs
+      # TODO see why I cannot use a case with the space returned by warrior.feel(stairs_direction)
+      if warrior.feel(stairs_direction).captive?
+        warrior.rescue!(stairs_direction)
+      elsif warrior.feel(stairs_direction).enemy?
+        warrior.attack!(stairs_direction)
+      else
+        warrior.walk!(stairs_direction)
+      end
     end
+  end
+
+  def bind_near_enemies(warrior)
+    unless @all_enemies_bound
+      [:forward, :left, :right, :backward].each do |direction|
+        if warrior.feel(direction).enemy?
+          warrior.bind!(direction)
+          return true
+        end
+      end
+      @all_enemies_bound = true
+    end
+    false
   end
 end
